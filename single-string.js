0248098f39f409bf24eeff6c4d87a422ed094cb1
Joe Woods <woodsja@mymail.vcu.edu>
2017-11-04 13:02:30 -0400

    fine, let's do it mark's way

--- single-string.js
+++ single-string.js
@@ -1,29 +1,29 @@
+
 var SingleStringPlugin = function(data, config){
     
     var that = this;
 
     that.bot = data.bot;
-
+    that.config = {}; 
+    
     Object.assign(that.config, data.config, config || {});
 
     phrases = {
-        "/shrug": "¯\_(ツ)_/¯",
+        "/shrug": "¯\\_(ツ)_/¯",
         "/shots": "SHOTS"
     };
 
     that.commands = {}
+
+    function generateReplaceFunction(replacement) {
+        return function(input, user) {
+            that.bot.emit("do:commandResponse", replacement);
+        }
+    }
  
-    for (var i = 0; i < Object.entries(phrases).length; i++) {
-
-        /* make array from each key/object pair in phrases, sequentially */
-        phrase_array = Object.entries(phrases)[i];
-
-        /* make function to evaluate */
-        eval_func = "(function(input, user) {that.bot.emit('do:commandResponse', '" + phrase_array[1] + "'); })";
-        
-        /* build function for each phrase in phrases */ 
-        that.commands[phrase_array[0]] = eval_func;
-    };
+    for (var key in phrases) {
+        that.commands[key] = generateReplaceFunction(phrases[key]);
+    }
 
     return that;
 };
