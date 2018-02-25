cd92e9b0ec59f617de5577eec44ce6e31844e93a
Mark Cicoria <mark@workingon.co>
2018-02-24 21:12:21 -0800

    update plugin to new format add uri encoding

--- dadjoke.js
+++ dadjoke.js
@@ -1,34 +1,40 @@
 var request = require("request");
 
+
+const 
+    _description = "Get a dad joke! Will give a random joke if you do not use a search term. - by @Captian Internet",
+    _help = {
+        "/dadjoke [search]": "Get a dad joke! Will give a random joke if you do not use a search term. - by @Captian Internet"
+    }
+;
+
 var DadJokePlugin = function (data) {
 
     var that = this;
 
     that.bot = data.bot;
 
-    that.help = {
-        "/dadjoke (search)": "Get a dad joke! Will give a random joke if you do not use a search term."
-    };
+    that.help = _help;
 
     function getJoke(searchURL,n) {
         request.get({
             url: searchURL,
             json: true,
-			limit: n,
-			headers: {
-				'User-Agent': 'JQBX.FM Bot'
-			}
+            limit: n,
+            headers: {
+                'User-Agent': 'JQBX.FM Bot'
+            }
         }, function (err, resp) {
             if (err) return that.bot.emit("error", err);
-            var str = "No data.";
+            var str = "No yolks found.";
 
             if (resp.body.joke) {
                 str = resp.body.joke;
             }else if(resp.body.results[0]){
-				var rand = Math.floor(Math.random() * resp.body.results.length);
-				console.log(resp.body.results[rand].joke);
-			}else {
-                str = "No jokes could be found."
+                var rand = Math.floor(Math.random() * resp.body.results.length);
+                // console.log(resp.body.results[rand].joke);
+            }else {
+                str = "No yolks could be found."
             }
 
             that.bot.emit("do:commandResponseExpandable", str, {
@@ -42,7 +48,7 @@ var DadJokePlugin = function (data) {
     that.commands = {
         "/dadjoke": function (input, user) {
             if (input) {
-                getJoke("https://icanhazdadjoke.com/search?term=" + input,20);
+                getJoke("https://icanhazdadjoke.com/search?term=" + encodeURIComponent(input),20);
             }
             else {
                 getJoke("https://icanhazdadjoke.com/",1);
@@ -53,4 +59,17 @@ var DadJokePlugin = function (data) {
     return that;
 };
 
+DadJokePlugin.help = _help;
+DadJokePlugin.description = _description;
+
 module.exports = DadJokePlugin;
+
+// This can be tested locally, like so:
+// var DJ = new DadJokePlugin({
+//     bot:{
+//         on: console.log,
+//         emit: console.log
+//     }
+// });
+
+// DJ.commands["/dadjoke"]("cat dog", {});
