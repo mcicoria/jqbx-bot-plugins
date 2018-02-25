fdcec29377f1fa50471aa3cb24b2e880b0e030b8
Mark Cicoria <mark@workingon.co>
2018-02-24 18:11:01 -0800

    working changes, with new interface for plugin

--- youtube.js
+++ youtube.js
@@ -1,27 +1,35 @@
-var request = require("request");
 var search = require('youtube-search');
+var config = require('../config');
+
+const 
+    _description = "Get a video URL from youtube matching the currently playing song. - by @Captian Internet",
+    _help = {
+        "/youtube (search)": "Get a video URL from youtube matching the currently playing song. Using arguments will return a video based on search terms. - by @Captian Internet"
+    }
+;
 
 var YouTubePlugin = function (data) {
-	
+    
     var that = this;
 
+    that.data = data;
     that.bot = data.bot;
+    that.room = data.room;
 
-    that.help = {
-        "/youtube (search)": "Get a video URL from youtube matching the currently playing song. Using arguments will return a video based on search terms."
-    };
+
+    that.help = _help;
  
-	var opts = {
-		maxResults: 1,
-		key: "AIzaSyDoSOhs5fzHSnqRvaQZ-k08cW2IrBiyx5s",
-		type: "video"
-	};
+    var opts = {
+        maxResults: 1,
+        key: config.youtube.key,
+        type: "video"
+    };
 
-    function searchYT(terms) {
-		
-		search(terms, opts, function(err, results) {
+    that.searchYT = function(terms) {
+        
+        search(terms, opts, function(err, results) {
             if (err) return that.bot.emit("error", err);
-			
+            
             var str = "No data.";
 
             if (results[0].link && results[0].title) {
@@ -34,29 +42,48 @@ var YouTubePlugin = function (data) {
             that.bot.emit("do:commandResponseExpandable", str, {
                 htmlMessage: str.split("\n").join("<br/><br/>")
             });
-	 
-			console.log(results);
-			
-			that.bot.emit("do:commandResponseExpandable", str, {
-                htmlMessage: str
-			});
 
-		});
-		
-		
+        });
+    
+    };
 
-    }
+    that.getArtistString = function(artists){
+        return artists.map(function(a){
+            return a.name
+        }).join(" ");
+    };
 
     that.commands = {
         "/yt": function (input, user) {
             if (!input) {
-                input = that.data.currentTrack.artists.name + " " + that.data.currentTrack.name;
+                input = that.getArtistString(that.data.currentTrack.artists) + " " + that.data.currentTrack.name;
             }
-			searchYT(input);
+            that.searchYT(input);
         }
     };
 
     return that;
 };
 
+YouTubePlugin.help = _help;
+YouTubePlugin.description = _description;
+
 module.exports = YouTubePlugin;
+
+// // This can be tested locally, like so:
+// var YT = new YouTubePlugin({
+//     bot:{
+//         on: console.log,
+//         emit: console.log
+//     },
+//     currentTrack: {
+//         artists: [
+//             {
+//                 name: "Cage The Elephant"
+//             }
+//         ],
+//         name: "James Brown"
+//     }
+// });
+
+// YT.commands["/yt"](null, {});
